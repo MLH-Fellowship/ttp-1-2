6c5afdc17ba27cf92649130ad19c90a3e391232d
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-24 17:19:55 -0400

    Added simple  input form

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { Component } from "react";
 
+class Input extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      college: "",
+      city: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default function Input(){
-  return(
-    <div>
-      <h1>Hello from input page!</h1>
-    </div>
-  )
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    alert(
+      "Values: " +
+        this.state.firstName +
+        " " +
+        this.state.lastName +
+        " " +
+        this.state.college +
+        " " +
+        this.state.city
+    );
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First name:
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Last name:
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          College:
+          <input
+            type="text"
+            name="college"
+            value={this.state.college}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          City:
+          <input
+            type="text"
+            name="city"
+            value={this.state.city}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
+
+export default Input;
