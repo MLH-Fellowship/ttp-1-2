b679e3fce7011948ed95656bd37f9ce7ce88f442
Sahal Asghar <sahaldos1@gmail.com>
2020-06-25 10:45:44 -0400

    Implement toggle button to allow for template component to display

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,80 +1,94 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Template from "./Template"
 
 class Input extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   firstName: "",
-    //   lastName: "",
-    //   college: "",
-    //   city: "",
-    // };
-    // this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      college: "",
+      city: "",
+      showTemplate: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleChange(event) {
-  //   this.setState({
-  //     [event.target.name]: event.target.value,
-  //   });
-  // }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
 
-  // handleSubmit(event) {
-  //   alert(
-  //     "Values: " +
-  //       this.state.firstName +
-  //       " " +
-  //       this.state.lastName +
-  //       " " +
-  //       this.state.college +
-  //       " " +
-  //       this.state.city
-  //   );
-  //   event.preventDefault();
-  // }
+  handleSubmit(event) {
+    alert(
+      "Values: " +
+        this.state.firstName +
+        " " +
+        this.state.lastName +
+        " " +
+        this.state.college +
+        " " +
+        this.state.city
+    );
+    event.preventDefault();
+  }
+
+
+  clickToggle = () => {
+    this.state.showTemplate === true ? this.setState({ showTemplate: false }) : this.setState({ showTemplate: true });
+  };
+
 
   render() {
+
+    let templates = this.state.showTemplate === true ? (<Template />) : (<form onSubmit={this.handleSubmit}>
+      <label>
+        First name:
+        <input
+          type="text"
+          name="firstName"
+          value={this.state.firstName}
+          onChange={this.handleChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          type="text"
+          name="lastName"
+          value={this.state.lastName}
+          onChange={this.handleChange}
+        />
+      </label>
+      <label>
+        College:
+        <input
+          type="text"
+          name="college"
+          value={this.state.college}
+          onChange={this.handleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          type="text"
+          name="city"
+          value={this.state.city}
+          onChange={this.handleChange}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>);
+
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <label>
-          First name:
-          <input
-            type="text"
-            name="firstName"
-            // value={this.state.firstName}
-            onChange={this.props.handleChange}
-          />
-        </label>
-        <label>
-          Last name:
-          <input
-            type="text"
-            name="lastName"
-            // value={this.state.lastName}
-            onChange={this.props.handleChange}
-          />
-        </label>
-        <label>
-          College:
-          <input
-            type="text"
-            name="college"
-            // value={this.state.college}
-            onChange={this.props.handleChange}
-          />
-        </label>
-        <label>
-          City:
-          <input
-            type="text"
-            name="city"
-            // value={this.state.city}
-            onChange={this.props.handleChange}
-          />
-        </label>
-        <Link to="/template"><input type="submit" value="Submit" /></Link>
-      </form>
+      <div>
+        {templates}
+        <button onClick={this.clickToggle}>Toggle</button>
+      </div>
     );
   }
 }
--- src/Components/Template.js
+++ src/Components/Template.js
@@ -1,45 +1,12 @@
 import React, { Component } from "react";
 import Input from "./Input";
 class Template extends Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      college: "",
-      city: "",
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    alert(
-      "Values: " +
-        this.state.firstName +
-        " " +
-        this.state.lastName +
-        " " +
-        this.state.college +
-        " " +
-        this.state.city
-    );
-    event.preventDefault();
-  }
-
+  
 
   render() {
     return (
       <div>
-        <Input handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
-        
+        <h1>Hello from template!</h1>
       </div>
     );
   }
