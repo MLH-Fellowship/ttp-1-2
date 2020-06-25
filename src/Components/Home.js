5f504cddb2b9e6c2f91548eb8d1113d78686c286
Alex Tan <AlexTAN331@outlook.com>
2020-06-25 12:58:12 -0400

    add background color and header

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,12 +6,20 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to our Portfolio Builder!</h1>
-        <h2>Need help building a portfolio?</h2>
-        <h2>
-          With our site you can create your resume in a few easy steps! Just
-          enter in your info and a resume will be built for you!
-        </h2>
+        <div className = "App-header">
+          <h2>Portfolio Builder</h2>
+        </div>
+
+        <div className="introduction">
+          <h1>Welcome to our Portfolio Builder!</h1>
+          <h2>Need help building a portfolio?</h2>
+
+          <h3>
+            With our site you can create your resume in a few easy steps! Just
+            enter in your info and a resume will be built for you!
+          </h3>
+        </div>
+
         <Link to="/input">
           <button>Click here to start</button>
         </Link>
