19cc086602ec126dea4d01f1f015b1fe17134fd1
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-25 11:26:37 -0400

    Added new fields

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Template from "./Template"
+import Template from "./Template";
 
 class Input extends Component {
   constructor(props) {
@@ -8,12 +8,18 @@ class Input extends Component {
     this.state = {
       firstName: "",
       lastName: "",
-      college: "",
       city: "",
+      phoneNum: "",
+      email: "",
+      summary: "",
+      major: "",
+      gpa: "",
+      activities: "",
+      description: "",
       showTemplate: false,
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    //    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -22,76 +28,152 @@ class Input extends Component {
     });
   }
 
-  handleSubmit(event) {
-    alert(
-      "Values: " +
-        this.state.firstName +
-        " " +
-        this.state.lastName +
-        " " +
-        this.state.college +
-        " " +
-        this.state.city
-    );
-    event.preventDefault();
-  }
-
+  // handleSubmit(event) {
+  //   alert(
+  //     "Values: " +
+  //       this.state.firstName +
+  //       " " +
+  //       this.state.lastName +
+  //       " " +
+  //       this.state.college +
+  //       " " +
+  //       this.state.city
+  //   );
+  //   event.preventDefault();
+  // }
 
   clickToggle = () => {
-    this.state.showTemplate === true ? this.setState({ showTemplate: false }) : this.setState({ showTemplate: true });
+    this.state.showTemplate === true
+      ? this.setState({ showTemplate: false })
+      : this.setState({ showTemplate: true });
   };
 
-
   render() {
+    let templates =
+      this.state.showTemplate === true ? (
+        <div>
+          <Template {...this.state} />
+          <button onClick={this.clickToggle}>Go Back</button>
+        </div>
+      ) : (
+        <form onSubmit={this.handleSubmit}>
+          <h2>Personal Information:</h2>
+          <label>
+            First name:
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Last name:
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            City:
+            <input
+              type="text"
+              name="city"
+              value={this.state.city}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Phone Number:
+            <input
+              type="text"
+              name="phoneNum"
+              value={this.state.phoneNum}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Professional Summary:
+            <input
+              type="text"
+              name="summary"
+              value={this.state.summary}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <h2>Educational Background</h2>
+          <label>
+            University:
+            <input
+              type="text"
+              name="college"
+              value={this.state.college}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Major:
+            <input
+              type="text"
+              name="major"
+              value={this.state.major}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            GPA:
+            <input
+              type="text"
+              name="gpa"
+              value={this.state.gpa}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Activities:
+            <input
+              type="text"
+              name="activities"
+              value={this.state.activities}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <label>
+            Description:
+            <input
+              type="text"
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br></br>
+          <input type="submit" value="Submit" onClick={this.clickToggle} />
+        </form>
+      );
 
-    let templates = this.state.showTemplate === true ? (<div>
-      <Template {...this.state} />
-      <button onClick={this.clickToggle}>Go Back</button></div>) 
-    : (<form onSubmit={this.handleSubmit}>
-      <label>
-        First name:
-        <input
-          type="text"
-          name="firstName"
-          value={this.state.firstName}
-          onChange={this.handleChange}
-        />
-      </label>
-      <label>
-        Last name:
-        <input
-          type="text"
-          name="lastName"
-          value={this.state.lastName}
-          onChange={this.handleChange}
-        />
-      </label>
-      <label>
-        College:
-        <input
-          type="text"
-          name="college"
-          value={this.state.college}
-          onChange={this.handleChange}
-        />
-      </label>
-      <label>
-        City:
-        <input
-          type="text"
-          name="city"
-          value={this.state.city}
-          onChange={this.handleChange}
-        />
-      </label>
-      <input type="submit" value="Submit" onClick={this.clickToggle}/>
-    </form>);
-
-    return (
-      <div>
-        {templates}
-      </div>
-    );
+    return <div>{templates}</div>;
   }
 }
 
--- src/Components/Template.js
+++ src/Components/Template.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 
 class Template extends Component {
-  
-
   render() {
     return (
       <div>
-        <h1>{this.props.firstName}</h1>
-        <h1>{this.props.lastName}</h1>
-        <h1>{this.props.college}</h1>
+        <h1>
+          {this.props.firstName} {this.props.lastName}
+        </h1>
         <h1>{this.props.city}</h1>
+        <h1>{this.props.phoneNum}</h1>
+        <h1>{this.props.email}</h1>
+        <h1>{this.props.summary}</h1>
+        <h1>{this.props.university}</h1>
+        <h1>{this.props.major}</h1>
+        <h1>{this.props.gpa}</h1>
+        <h1>{this.props.activities}</h1>
+        <h1>{this.props.description}</h1>
       </div>
     );
   }
