e4563d1ae92d7a317988dbdb32fbb6b1475dc9fa
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-26 10:44:03 -0400

    Clean up code:

--- src/Components/Template.js
+++ src/Components/Template.js
@@ -69,22 +69,6 @@ class Template extends Component {
             </CardText>
           </CardBody>
         </Card>
-
-        {/* <h1>Personal Information</h1>
-        <h1>
-          {this.props.firstName} {this.props.lastName}
-        </h1>
-        <h1>{this.props.city}</h1>
-        <h1>{this.props.phoneNum}</h1>
-        <h1>{this.props.email}</h1>
-        <h1>{this.props.summary}</h1>
-        <h1>Educational Background</h1>
-        <h1>{this.props.university}</h1>
-        <h1>{this.props.major}</h1>
-        <h1>{this.props.degree}</h1>
-        <h1>{this.props.gpa}</h1>
-        <h1>{this.props.activities}</h1>
-        <h1>{this.props.description}</h1> */}
       </div>
     );
   }
